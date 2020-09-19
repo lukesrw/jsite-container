ca9b9f29ab2ebf408b4eb19949250e5d5fbbed11
lukesrw <lukesrw+github@gmail.com>
2020-09-19 22:57:16 +0100

    Added module options

--- src/interfaces/jsite.ts
+++ src/interfaces/jsite.ts
@@ -1,5 +1,10 @@
+import * as Generic from "../interfaces/generic";
+
 export interface Options {
     [name: string]: any;
     abs: string;
+    modules: {
+        [module: string]: Generic.Object;
+    };
     production: boolean;
 }
