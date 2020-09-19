5be7ac1ec5a5385782138aa31ac34cb163d425a4
lukesrw <lukesrw+github@gmail.com>
2020-09-19 22:58:39 +0100

    Improved database implement

--- src/modules/database/index.ts
+++ src/modules/database/index.ts
@@ -1,10 +1,30 @@
 import { JSite } from "../..";
 import * as path from "path";
 import { ModuleInfo } from "../../interfaces/module";
+import * as Generic from "../../interfaces/generic";
+
+interface QueryInterface extends Generic.Object {
+    callback: Function;
+}
 
 function setup(jsite: JSite): JSite {
     if (!Object.prototype.hasOwnProperty.call(jsite.custom, "database")) {
         jsite.custom.database = {};
+        jsite.on("database:select", promises => {
+            promises.push(async (options: QueryInterface) => {
+                let callback = options.callback;
+                delete options.callback;
+
+                Object.keys(jsite.custom.database).forEach(driver => {
+                    switch (driver) {
+                        case "mysql":
+                            return jsite.custom.database[driver].query(options, callback);
+                    }
+                });
+
+                return options;
+            });
+        });
     }
 
     return jsite;
@@ -12,16 +32,20 @@ function setup(jsite: JSite): JSite {
 
 export function mysql(jsite?: JSite): ModuleInfo {
     if (jsite) {
-        setup(jsite).on("jsite:start", promises => {
+        setup(jsite).on("jsite:reload", promises => {
             promises.push(async () => {
                 const mysql = require("mysql2");
 
-                jsite.custom.database.mysql = mysql.createPool({
-                    database: "eds",
-                    host: "localhost",
-                    password: "",
-                    user: "root"
-                });
+                jsite.custom.database.mysql = mysql.createPool(
+                    Object.assign(
+                        {
+                            host: "localhost",
+                            password: "",
+                            user: "root"
+                        },
+                        jsite.options.modules.database.mysql
+                    )
+                );
             });
         });
     }
@@ -34,7 +58,7 @@ export function mysql(jsite?: JSite): ModuleInfo {
 
 export function sqlite(jsite?: JSite): ModuleInfo {
     if (jsite) {
-        setup(jsite).on("jsite:start", promises => {
+        setup(jsite).on("jsite:reload", promises => {
             promises.push(async () => {
                 const sqlite = require("sqlite3").verbose();
 
