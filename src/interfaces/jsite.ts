553d085432243c3eceee923e2a28d784c758561f
lukesrw <lukesrw+github@gmail.com>
2020-10-13 21:59:54 +0100

    Simplified interfaces, removed duplicate

--- src/interfaces/jsite.ts
+++ src/interfaces/jsite.ts
@@ -1,19 +1,8 @@
 import * as Generic from "../interfaces/generic";
 
 export interface Options {
-    [name: string]: any;
-    abs: string;
-    modules: {
-        [module: string]: Generic.Object;
-    };
-    production?: boolean;
-}
-
-export interface OptionsInput {
-    [name: string]: any;
     abs?: string;
-    modules?: {
-        [module: string]: Generic.Object;
-    };
+    custom?: { [module: string]: Generic.Object };
     production?: boolean;
+    modules?: [string, string][];
 }
