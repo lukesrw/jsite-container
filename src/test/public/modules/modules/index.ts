9a7879a708e47330eb8756ac1673c552a0d048b9
lukesrw <lukesrw+github@gmail.com>
2020-10-13 22:43:42 +0100

    Major update to finish initial modules

--- src/test/public/modules/modules/index.ts
+++ src/test/public/modules/modules/index.ts
@@ -1,31 +1,39 @@
 import { JSite } from "../../../../index";
+import * as Generic from "../../../../interfaces/generic";
 import { ModuleInfo } from "../../../../interfaces/module";
-
-interface ModuleRecordInterface {
-    module_stub: string;
-    module_function: string;
-}
 
 export function modules(jsite?: JSite): ModuleInfo {
     if (jsite) {
-        jsite.on("jsite:start", promises => {
-            promises.push(async () => {
-                await jsite.sendEmit("database:all", {
-                    callback: (error: string | null, modules: ModuleRecordInterface[]) => {
-                        let set_modules = jsite.modules;
-
-                        if (error) throw new Error(error);
-
-                        (modules || []).forEach(module => {
-                            set_modules.push([module.module_stub, module.module_function]);
-                        });
-
-                        jsite.setModules(...set_modules);
-                        jsite.reload();
-                    },
-                    sql: "SELECT module_stub, module_function FROM modules WHERE module_is_active = 1"
-                });
+        jsite.on("modules:all", promises => {
+            promises.push(async (data: Generic.Object) => {
+                data = Object.assign(
+                    data,
+                    await jsite.sendEmit("database:all", {
+                        sql: "SELECT * FROM modules WHERE module_is_active = 1"
+                    })
+                );
+                data.results = data.results || [];
             });
+        });
+
+        jsite.on("modules:reload", promises => {
+            promises.push(async () => {
+                let modules = await jsite.sendEmit("modules:all");
+
+                jsite.setModules(
+                    ...jsite.getOption("modules", []).concat(
+                        modules.results.map((module: Generic.Object) => {
+                            return [module.module_stub, module.module_function];
+                        })
+                    )
+                );
+
+                return jsite.reload();
+            });
+        });
+
+        jsite.on("jsite:start", promises => {
+            promises.push(async () => await jsite.sendEmit("modules:reload"));
         });
     }
 
