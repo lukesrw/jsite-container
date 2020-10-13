e586d8fadef238d909316becaa4974d2883ad303
lukesrw <lukesrw+github@gmail.com>
2020-10-13 22:44:37 +0100

    Updated with preload modules

--- src/test/index.ts
+++ src/test/index.ts
@@ -1,3 +1,21 @@
-import { JSite } from "../";
+import { JSite } from "../index";
 
-new JSite().start();
+let server = new JSite({
+    custom: {
+        database: {
+            mysql: {
+                charset: "UTF8MB4_UNICODE_CI",
+                database: "container",
+                dateStrings: true,
+                multipleStatements: true,
+                namedPlaceholders: true
+            }
+        }
+    },
+    modules: [
+        ["modules", "modules"],
+        ["database", "mysql"]
+    ]
+});
+
+server.start();
