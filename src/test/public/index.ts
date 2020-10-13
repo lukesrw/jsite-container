0a4b2b101f34405a9dea5dd64519705c75a4df0e
lukesrw <lukesrw+github@gmail.com>
2020-10-13 22:43:32 +0100

    Removed console log

--- src/test/public/index.ts
+++ src/test/public/index.ts
@@ -3,9 +3,6 @@ import { JSite } from "../..";
 export = async (_1: unknown, jsite: JSite) => {
     let test = await jsite.sendEmit("modules:all");
 
-    console.log("public/");
-    console.log(test);
-
     return {
         data: test
     };
