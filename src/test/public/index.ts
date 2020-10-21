e9ca67700c934d743030f98d4a04b762caf2d0d5
lukesrw <lukesrw+github@gmail.com>
2020-10-21 22:22:38 +0100

    Removed internal modules

--- src/test/public/index.ts
+++
@@ -1,9 +0,0 @@
-import { JSite } from "../..";
-
-export = async (_1: unknown, jsite: JSite) => {
-    let test = await jsite.sendEmit("modules:all");
-
-    return {
-        data: test
-    };
-};
--- src/test/public/modules/routes/interfaces/index.ts
+++
@@ -1,6 +0,0 @@
-export interface Rule {
-    regex?: string | RegExp;
-    flags?: string;
-    file: string;
-    matches: RegExpMatchArray[];
-}
